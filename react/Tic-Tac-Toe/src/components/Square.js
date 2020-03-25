2b7626f75757d3eeb49caf71e2f9706b9fa21b9c
leyuri <leyuri97@gmail.com>
2020-03-26 00:11:03 +0900

    Add Square Component

---
+++ react/Tic-Tac-Toe/src/components/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Square extends React.Component {
+
+    render() {  //JSX
+        return (
+            <button className='btn'>Square </button>
+        );
+    }
+}
+
+export default Square;
