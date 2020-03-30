bcd0dc9d2cb20f77980f092162d3d9ae8eebc8aa
leyuri <leyuri97@gmail.com>
2020-03-30 17:46:55 +0900

    Add calculateWinner

--- react/Tic-Tac-Toe/src/components/Board.js
+++ react/Tic-Tac-Toe/src/components/Board.js
@@ -15,7 +15,7 @@ class Board extends Component {
     handleClick(i) {
         const { squares, xIsNext } = this.state;
         const arr = squares.slice();
-        if( arr[i]){
+        if( arr[i] || this.calculateWinner(squares)){
             return;
             //이미 수를 놓으면 아무것도 할 수 없도록
         }
@@ -48,9 +48,40 @@ class Board extends Component {
         )
     }
 
+    calculateWinner(squares){
+
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for(let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i]; 
+            if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]){
+                return squares[a];
+            } 
+        }
+        return null;
+        //이긴 사람이 아직 없을 때
+    }
 
     render() {
-        const status = `Next player: ${this.state.xIsNext? 'X':'O'}`;
+        const winner = this.calculateWinner(this.state.squares);
+        let status;
+        /*let 과 const 의 차이점은 변수의 immutable 여부이다. 
+        let 은 변수에 재할당이 가능하지만, const 는 변수 재선언, 
+        재할당 모두 불가능하다. */
+        if (winner) {
+            status = `Winner: ${winner}`;
+        } else {
+            status = `Next player: ${this.state.xIsNext? 'X':'O'}`;
+        }
 
         return (
             <div>
