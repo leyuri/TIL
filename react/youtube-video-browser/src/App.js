bae37ca6e2046d1b038fce8ad5aba9957b99b2e7
leyuri <leyuri97@gmail.com>
2020-03-31 19:57:11 +0900

    Add VideoItem Component

--- react/youtube-video-browser/src/App.js
+++ react/youtube-video-browser/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 import * as youtubesearch from "youtube-search";
 import SearchBar from './components/search_bar';
+import VideoItem from './components/video_item';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
 
 // AIzaSyBa4oPbrdMSLkzpHVtKQdbtB3eJitVUJZA
 
@@ -13,7 +17,8 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      video: [],
+      term: '명지대',
+      videos: [],
       selectedVideo: null
     }
   }
@@ -22,12 +27,13 @@ class App extends React.Component {
     this.search();
   }
 
-  search(){
-    youtubesearch("BTS", {key: GOOGLE_KEY}, (err, results) => {
+  search(term){
+    this.setState({term: term});
+    youtubesearch(term, {key: GOOGLE_KEY}, (err, results) => {
       if(err) return console.log(err);
       console.log(results);
       this.setState({
-        video: results,
+        videos: results,
         selectedVideo: results[0]
       })
       // console.log(results);
@@ -38,14 +44,19 @@ class App extends React.Component {
 
     return (
       <div className="App container mt-3">
-        <SearchBar/>
+        <SearchBar term={this.state.term} onChange={(term) => {
+          this.search(term);
+        }}/>
         <div className="row">
           <div className="col-8">
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-  
+          <VideoDetail video={this.state.selectedVideo}/>
           </div>
           <div className="col-4">
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            <VideoList videos={this.state.videos}
+              onItemSelect={(video) => {
+                this.setState({selectedVideo: video});
+              }}
+            />
   
           </div>
         </div>
---
+++ react/youtube-video-browser/src/components/video_list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import VideoItem from './video_item';
+
+
+class VideoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return(
+
+            <div className="videoList">
+            {
+                this.props.videos.map(item => (<VideoItem key={item.id} video={item}/>))
+            }
+
+            </div>
+
+
+           
+        )
+    }
+
+}
+
+export default VideoList;
