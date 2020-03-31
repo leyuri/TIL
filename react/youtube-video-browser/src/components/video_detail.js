4f5d0e749bb664fa92c490b24a4589d3bc0d7061
leyuri <leyuri97@gmail.com>
2020-03-31 19:56:47 +0900

    Add VideoDetail Component

--- react/youtube-video-browser/src/components/video_detail.js
+++ react/youtube-video-browser/src/components/video_detail.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import VideoList from './video_list';
 
-function video_detail() {
+
+export default function VideoDetail({video}) {
+
+
+    if (!video) {
+        return (<div className="text-center">No video</div>)
+    }
+
     return (
-        <div>
-            
-        </div>
-    )
+        <div className="embed-responsive embed-responsive-16by9">
+        <iframe className="embed-responsive-item" title="video"
+        src={`https://www.youtube.com/embed/${video.id}`}
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+      </div>
+    );
 }
 
-export default video_detail
